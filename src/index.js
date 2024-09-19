80ffea311d22b29195bbb45edb41c87bc9cc67cb
= <=>
2024-09-19 15:14:33 +0700

    Delete unused files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import TodoApp from './components/TodoApp';
-import './App.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
